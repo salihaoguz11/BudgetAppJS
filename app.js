6f60b24fd71dfc1342691401f441900cc619f391
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-11-02 11:15:21 +0000

    addExpense func has been declared

--- app.js
+++ app.js
@@ -68,3 +68,32 @@ function submitExpenseForm() {
     showBalance();
   }
 }
+
+function addExpense() {
+  const expenseDiv = document.createElement("div");
+  expenseDiv.classList.add("expense");
+  expenseDiv.innerHTML = `
+  <div
+  class="expense-item d-flex justify-content-between align-items-baseline"
+>
+  <h6 class="expense-title mb-0 text-uppercase list-item">
+    title
+  </h6>
+
+  <h5 class="expense-amount mb-0 list-item">amount</h5>
+  <div class="expense-icons list-item">
+    <a
+      href="#"
+      class="edit-icon mx-2"
+      data-id="${expense.id}"
+    >
+      <i class="fas fa-edit"></i>
+    </a>
+    <a href="#" class="delete-icon" data-id="${expense.id}">
+      <i class="fas fa-trash"></i>
+    </a>
+  </div>
+</div>
+  
+  `;
+}
